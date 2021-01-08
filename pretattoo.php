e0afc695ba4007abfe1850047307f29b730c5aa8
André Fernandez <andrefteodoro@gmail.com>
2021-01-08 13:59:10 -0300

    Update pretattoo.php

--- pretattoo.php
+++ pretattoo.php
@@ -1,21 +1,17 @@
 
 <?php
 
-echo 'oi papis'; 
+echo 'oi papis 2<br>'; 
 
-$to      = 'andrefteodoro@gmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$email_usuario = "andrefteodoro@gmail.com";
+$email_remetente = "andre@donttouch.com.br";
 
-if(mail($to, $subject, $message, $headers)){ 
-    echo 'Email has sent successfully.'; 
-}else{ 
-   echo 'Email sending failed.'; 
-}
-
+$headers = "MIME-Version: 1.1\n";
+$headers .= "Content-type: text/plain; charset=iso-8859-1\n";
+$headers .= "From: $email_remetente\n"; // remetente
+$headers .= "Return-Path: $email_remetente\n"; // return-path
+$headers .= "Reply-To: $email_usuario\n"; // Endereço (devidamente validado) que o seu usuário informou no contato
+$envio = mail("destinatario@algum-email.com", "Assunto", "Mensagem", $headers, "-f$email_remetente");
 ?>
 
 
