793e8f8493d9d163ca31e0a1b6caec6eba29687e
André Fernandez <andrefteodoro@gmail.com>
2021-01-08 13:21:47 -0300

    primeiro
    online

---
+++ pos.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Pré-Tattoo</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body style="margin: 0; padding: 0;">
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+	<tr>
+		<td>
+   			<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+				<tr>
+					<td align="center" bgcolor="#000000" style="padding: 20px 0 30px 0;">
+ 						<img src="http://15.228.36.137/imgs/topo.jpg" alt="E-mail Pré-tattoo Don't Touch" width="600" height="284" style="display: block;" />
+					</td>
+				</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-thank.jpg" alt="Tatuagem dói, mas nem tanto." width="80" height="80" style="display: inline-block;" />
+			  					</td>
+			  					<td>
+			  						<p style="font-family:verdana">A gente não disse que passaria rápido? Agora vem uma das partes mais importantes pra sua tatuagem ficar linda durante anos: o seu cuidado com ela daqui pra frente! Calma que nós somos experts e vamos te guiar lindamente nesse processo!</p>
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+
+
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+
+
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<p style="font-family:verdana">Primeiro de tudo, NUNCA, JAMAIS, EM TEMPO ALGUM entre na praia, piscinas, banheiras, cachoeiras, balde ou aquele banho de mangueira na laje! Por pelo menos um mês você deve se manter igual um gato com medo de água (tomar banho em casa pode! Aliás, deve, já chegamos lá), e como um Vampiro com medo de sol! O sol durante a sua tatuagem recém feita, é um vilão mais sinistro do que o Thanos com a Manopla do Infinito!</p>
+			  					</td>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-dor.jpg" alt="Tatuagem dói, mas nem tanto." width="80" height="80" style="display: inline-block;" />
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-roupa.jpg" alt="Tatuagem dói, mas nem tanto." width="90" height="90" style="display: inline-block;" />
+			  					</td>
+			  					<td>
+		  							<p style="font-family:verdana">Recomendamos que você lave suavemente a sua tatuagem com água fria! Sim, faz esse sacrifício aí! Use um sabonete antibacteriano e faça movimentos leves, e pra secar, somente encoste papel toalha nela, até que fique sequinha. Depois você vai passar uma camada FININHA E TRANSPARENTE da pomada que o seu tatuador indicou na sessão.</p>
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<p style="font-family:verdana">Posso comer Bacon? E Chocolate? E frituras? E bacon frito recheado com chocolate? Nessa aí vem mais um pouco de sacrifício pra quem curte essas iguarias. Não é que você vai explodir e a sua tatuagem vai sair voando se você comer alguma dessas coisas, mas uma alimentação saudável ajuda DEMAIS na cicatrização. Melhor previnir, né não?</p>
+			  					</td>
+			  					<td>
+		  							<img src="http://15.228.36.137/imgs/icone-alcool.jpg" alt="Tatuagem dói, mas nem tanto." width="70" height="70" style="display: inline-block;" />
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-misterio.jpg" alt="Tatuagem dói, mas nem tanto." width="70" height="70" style="display: inline-block;" />
+			  					</td>
+			  					<td>
+			  						<p style="font-family:verdana">A tatuagem fica completamente cicatrizada entre 3 a 4 semanas, depende de como seu corpinho vai reagir. Depois daquela casca fininha sair SOZINHA, não pode arrancar casquinha viu? Ai ai ai! Você já vai poder começar a passar hidratante e filtro solar direto, pois ali vai estar uma nova camada de pele, e uma tatuagem linda e bem feita! Lembre-se: tatuagem é uma parceria entre você e seu tatuador! Seguindo essas dicas básicas, CERTEZA que essa dupla vai ser mais do que vitoriosa!</p>
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<p style="font-family:verdana">E se eu precisar de um retoque? Relaxa, caso você fique na dúvida se existe ou não está necessidade, chama a gente que avaliamos e caso exista você tem todo esse direito sem custo algum. </p>
+			  					</td>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-valet.jpg" alt="Tatuagem dói, mas nem tanto." width="70" height="70" style="display: inline-block;" />
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-problema.jpg" alt="Tatuagem dói, mas nem tanto." width="70" height="70" style="display: inline-block; margin-right: 10px;" />
+			  					</td>
+			  					<td>
+			  						<p style="font-family:verdana">Nós da DT agradecemos demais a confiança e a preferência pela nossa casa! Torcemos pra que você volte logo! Tem alguma coisa que você queria ver aqui da próxima vez? Responde pra gente que é muito importante!</p>
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			 		<td style="text-align: center; padding: 20px 20px 10px 20px;">
+			 			<a href="https://api.whatsapp.com/send?phone=5511966551515" target="_blank"><img src="http://15.228.36.137/imgs/icone-whats.jpg" alt="Whats estúdio: +5511966551515" width="50" height="50" style="display: inline-block; margin: 0px 30px 0px 0px;" /></a>
+
+			 			<a href="http://instagram.com/studiodonttouch" target="_blank"><img src="http://15.228.36.137/imgs/icone-insta.jpg" alt="http://instagram.com/studiodonttouch" width="50" height="50" style="display: inline-block; margin: 0px 30px 0px 0px;" /></a>
+
+			 			<a href="https://www.facebook.com/studiodonttouch/" target="_blank"><img src="http://15.228.36.137/imgs/icone-facebook.jpg" alt="https://www.facebook.com/studiodonttouch/" width="50" height="50" style="display: inline-block; margin: 0px 0px 0px 0px;" /></a>
+			 		</td>
+			 	</tr>
+
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+
+			 	<tr>
+			 		<td style="text-align: center;">
+			 			<a href="https://donttouch.com.br/" target="_blank" title="Don't Touch, Fresh Ink, Tattoo Studio">
+			 			<img src="http://15.228.36.137/imgs/logo-preto.jpg" alt="https://www.facebook.com/studiodonttouch/" 
+			 			width="120" 
+			 			height="230" />
+			 			</a>
+			 		</td>
+			 	</tr>
+
+			</table>
+  		</td>
+ 	</tr>
+</table>
+</body>
+</html>
---
+++ pretattoo.php
@@ -0,0 +1,230 @@
+<?php
+
+
+$htmlContent = ' 
+    <html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Pré-Tattoo</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body style="margin: 0; padding: 0;">
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+	<tr>
+		<td>
+   			<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+				<tr>
+					<td align="center" bgcolor="#000000" style="padding: 20px 0 30px 0;">
+ 						<img src="http://15.228.36.137/imgs/topo.jpg" alt="E-mail Pré-tattoo Dont Touch" width="600" height="284" style="display: block;" />
+					</td>
+				</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-thank.jpg" alt="Tatuagem dói, mas nem tanto." width="80" height="80" style="display: inline-block;" />
+			  					</td>
+			  					<td>
+			  						<p style="font-family:verdana">Que maneiro que você fechou sua tatuagem com a gente! Pra te provar que não somos robozinhos doidos, e que queremos seu bem estar do início até o fim da sua tatuagem, aqui vão algumas dicas pra você se sentir mais confortável na sua sessão:</p>
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+
+
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+
+
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<p style="font-family:verdana">Tatuagem dói, mas não se preocupe que não é nenhum bicho papão! Pra se distrair você pode trazer seu livro preferido, fones de ouvido pra viajar nas músicas, assistir uma série ou filme, que num piscar de olhos sua arte estará na sua pele, sem crise!</p>
+			  					</td>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-dor.jpg" alt="Tatuagem dói, mas nem tanto." width="80" height="80" style="display: inline-block;" />
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-roupa.jpg" alt="Tatuagem dói, mas nem tanto." width="90" height="90" style="display: inline-block;" />
+			  					</td>
+			  					<td>
+		  							<p style="font-family:verdana">Venha pra sessão com roupas confortáveis, bem alimentado e tranquilo! Nossa equipe é treinada para deixar você o mais feliz possível.</p>
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<p style="font-family:verdana">Se consome álcool, tente não beber até dois dias antes da sua tatuagem. Existe um mito de que se você beber, vai sentir menos dor. Ledo engano gafanhoto! O álcool deixa você mais sensível a dor, além de fazer sua corrente sanguínea correr mais rápido, e assim você vai sangrar bem mais do que o normal.</p>
+			  					</td>
+			  					<td>
+		  							<img src="http://15.228.36.137/imgs/icone-alcool.jpg" alt="Tatuagem dói, mas nem tanto." width="70" height="70" style="display: inline-block;" />
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-misterio.jpg" alt="Tatuagem dói, mas nem tanto." width="70" height="70" style="display: inline-block;" />
+			  					</td>
+			  					<td>
+			  						<p style="font-family:verdana">Não tem mistério né? Siga essas dicas básicas que sua sessão já tá marcada! Em breve você saberá como é ser tatuado no estúdio mais humanizado do Brasil!</p>
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<p style="font-family:verdana">Nosso espaço conta com serviço de valet. Nosso profissional está disponível para te atender no período das 10 horas até as 20 horas, o custo é de R$ 25,00 o dia todo. As formas de pagamento são; cartão de débito, crédito e dinheiro.</p>
+			  					</td>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-valet.jpg" alt="Tatuagem dói, mas nem tanto." width="70" height="70" style="display: inline-block;" />
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-problema.jpg" alt="Tatuagem dói, mas nem tanto." width="70" height="70" style="display: inline-block; margin-right: 10px;" />
+			  					</td>
+			  					<td>
+			  						<p style="font-family:verdana">Caso haja a necessidade de alteração de data por qualquer imprevisto que possa ocorrer, pedimos a gentileza de nos avisar num prazo de 72 horas de antecedência. Nossos artistas são bem concorridos e separamos o dia especialmente para você além de preparar todo o material e artes para que sua experiência seja perfeita.</p>
+			  					</td>
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			  		<td bgcolor="#ffffff" style="padding: 20px 20px 20px 20px;">
+			  			<table>
+			  				<tr>
+			  					<td>
+			  						<p style="font-family:verdana">O caminho da felicidade é: Av Divino Salvador, 548. Planalto Paulista. Se quiser ver do Google Maps, é só <br><a href="https://www.google.com.br/maps/place/Av.+Divino+Salvador,+548+-+Planalto+Paulista,+S%C3%A3o+Paulo+-+SP,+04078-012/@-23.6073087,-46.6592159,17z/data=!3m1!4b1!4m5!3m4!1s0x94ce5a1140c03ef1:0x98dfb8b65efbe2e2!8m2!3d-23.6073136!4d-46.6570272" style="color: #000000;" target="_blank">clicar aqui</a> que tá tudo explicadinho.</p>
+			  					</td>
+			  					<td>
+			  						<img src="http://15.228.36.137/imgs/icone-mapa.jpg" alt="Tatuagem dói, mas nem tanto." width="70" height="70" style="display: inline-block;" />
+			  					</td>
+			  					
+			  				</tr>
+			  			</table>
+			  		</td>
+			 	</tr>
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+			 	<tr>
+			 		<td style="text-align: center; padding: 20px 20px 10px 20px;">
+			 			<a href="https://api.whatsapp.com/send?phone=5511966551515" target="_blank"><img src="http://15.228.36.137/imgs/icone-whats.jpg" alt="Whats estúdio: +5511966551515" width="50" height="50" style="display: inline-block; margin: 0px 30px 0px 0px;" /></a>
+
+			 			<a href="http://instagram.com/studiodonttouch" target="_blank"><img src="http://15.228.36.137/imgs/icone-insta.jpg" alt="http://instagram.com/studiodonttouch" width="50" height="50" style="display: inline-block; margin: 0px 30px 0px 0px;" /></a>
+
+			 			<a href="https://www.facebook.com/studiodonttouch/" target="_blank"><img src="http://15.228.36.137/imgs/icone-facebook.jpg" alt="https://www.facebook.com/studiodonttouch/" width="50" height="50" style="display: inline-block; margin: 0px 0px 0px 0px;" /></a>
+			 		</td>
+			 	</tr>
+
+			 	<tr>
+			 		<td>
+			 			<hr style="height:1px; border:none; color:#000; background-color:#000;">
+			 		</td>
+			 	</tr>
+
+			 	<tr>
+			 		<td style="text-align: center;">
+			 			<a href="https://donttouch.com.br/" target="_blank" title="Dont Touch, Fresh Ink, Tattoo Studio">
+			 			<img src="http://15.228.36.137/imgs/logo-preto.jpg" alt="https://www.facebook.com/studiodonttouch/" 
+			 			width="120" 
+			 			height="230" />
+			 			</a>
+			 		</td>
+			 	</tr>
+
+			</table>
+  		</td>
+ 	</tr>
+</table>
+</body>
+</html>'; 
+
+
+
+
+$to      = $_POST['email'];
+$subject = 'Dicas pré Tattoo, Dont Touch';
+$message = $htmlContent; 
+$headers = "MIME-Version: 1.0" . "\r\n"; 
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+$headers .= 'From: social@donttouch.com.br' . "\r\n" .
+    'Reply-To: social@donttouch.com.br' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+if(mail($to, $subject, $message, $headers)){ 
+    echo 'Email has sent successfully.'; 
+}else{ 
+   echo 'Email sending failed.'; 
+}
+
+?>
